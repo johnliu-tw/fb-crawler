adbabb999d1832eac9ced8539fb304cdc286e74b
johnliu <johnliutw@hotmail.com>
2020-01-11 22:01:31 +0800

    broke mechanism

---
+++ broke_auth.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
+from bs4 import BeautifulSoup
+import os
+import time
+
+options = Options()
+options.add_argument("--disable-notifications")
+options.add_argument('--headless')
+driver = webdriver.Chrome(os.getcwd() + '/chromedriver', chrome_options=options)
+driver.get('https://www.facebook.com')
+driver.set_window_size(1024, 768)
+
+username = driver.find_elements_by_css_selector("input[name=email]")[0]
+username.send_keys(os.getenv("EMAIL"))
+password = driver.find_elements_by_css_selector("input[name=pass]")[0]
+password.send_keys(os.getenv("PASSWORD"))
+login_button = driver.find_elements_by_css_selector("input[type=submit]")[0]
+login_button.click()
+
+keepGo = driver.find_elements_by_css_selector("#checkpointSubmitButton")[0]
+keepGo.click()
+time.sleep(2)
+
+radio = driver.find_elements_by_css_selector("input[name=verification_method]~ span")[1]
+radio.click()
+
+keepGo = driver.find_elements_by_css_selector("#checkpointSubmitButton")[0]
+keepGo.click()
+
+keepGo = driver.find_elements_by_css_selector("#checkpointSubmitButton")[0]
+keepGo.click()
+time.sleep(2)
