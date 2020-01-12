b36c455ce4d383d69ac890a95c6bddc5b4d08849
John <john831118@gmail.com>
2020-01-12 08:06:56 +0000

    make broke auth auto

--- broke_auth.py
+++ broke_auth.py
@@ -8,27 +8,37 @@ load_dotenv()
 
 def check_auth(driver):
   keepGo = driver.find_elements_by_css_selector("#checkpointSubmitButton")
-  if(len(keepGo > 0)):
+  if(len(keepGo) > 0):
     keepGo[0].click()
-    time.sleep(2)
+    time.sleep(5)
 
-    radio = driver.find_elements_by_css_selector("input[name=verification_method]~ span")[1]
+    radio = driver.find_elements_by_css_selector("input[name=verification_method]~ span")[0]
     radio.click()
 
     keepGo = driver.find_elements_by_css_selector("#checkpointSubmitButton")[0]
     keepGo.click()
+    
+    count = 0
+    is_filled = False
 
-    check = input('確認在手機解鎖後，請輸入 start ：')
-    if (check == 'start'):
-      keepGo = driver.find_elements_by_css_selector("#checkpointSubmitButton")[0]
-      keepGo.click()
-      time.sleep(2)
-
-      username = driver.find_elements_by_css_selector("input[name=email]")[0]
-      username.send_keys(os.getenv("EMAIL"))
-      password = driver.find_elements_by_css_selector("input[name=pass]")[0]
-      password.send_keys(os.getenv("PASSWORD"))
-      login_button = driver.find_elements_by_css_selector("input[type=submit]")[0]
-      login_button.click()
+    while count < 20 and is_filled == False:
+      keepGo = driver.find_elements_by_css_selector("#checkpointSubmitButton")
+      if (len(keepGo) > 0):
+        keepGo[0].click()
+        time.sleep(2)
+      
+      username = driver.find_elements_by_css_selector("input[name=email]")
+      if(len(username) > 0):
+        username[0].send_keys(os.getenv("EMAIL"))
+        password = driver.find_elements_by_css_selector("input[name=pass]")[0]
+        password.send_keys(os.getenv("PASSWORD"))
+        login_button = driver.find_elements_by_css_selector("input[type=submit]")[0]
+        driver.save_screenshot('img.png')
+        login_button.click()
+        is_filled = True
+      else: 
+        count += 1
+        print('try again')
+        time.sleep(5)
 
 
